722056a81711d30d6a275666add16a39147434ed
rafaelff97 <rFael_f_fernandes@hotmail.com>
2020-11-30 15:21:51 +0000

    Atualização 2

--- projeto_tematico/resources/views/adicionar-produto.blade.php
+++ projeto_tematico/resources/views/adicionar-produto.blade.php
@@ -41,32 +41,32 @@
 
             <div class="form-group">
                 <label>Sinónimos</label>
-                <input type="email" class="form-control" id="" placeholder="Digite os sinónimos...">
+                <input type="text" class="form-control" id="" placeholder="Digite os sinónimos...">
             </div>
             <div class="form-group">
                 <label>Unidade</label>
-                <input type="email" class="form-control" id="" placeholder="Digite a unidade...">
+                <input type="text" class="form-control" id="" placeholder="Digite a unidade...">
             </div>
             <!-- display none - lado esquerdo -->
             <div class="quimico" style="display:block;">
                 <div class="form-group">
                     <label>Fórmula</label>
-                    <input type="email" class="form-control" id="" placeholder="Digite a fórmula...">
+                    <input type="text" class="form-control" id="" placeholder="Digite a fórmula...">
                 </div>
 
                 <div class="form-group">
                     <label>Peso Molecular</label>
-                    <input type="email" class="form-control" id="" placeholder="Digite o peso molecular...">
+                    <input type="text" class="form-control" id="" placeholder="Digite o peso molecular...">
                 </div>
 
                 <div class="form-group">
                     <label>Nº CAS</label>
-                    <input type="email" class="form-control" id="" placeholder="Digite o Nº CAS...">
+                    <input type="text" class="form-control" id="" placeholder="Digite o Nº CAS...">
                 </div>
 
                 <div class="form-group">
                     <label>Condição de armazenamento</label>
-                    <input type="email" class="form-control" id="" placeholder="Digite a condição de armazenamento...">
+                    <input type="text" class="form-control" id="" placeholder="Digite a condição de armazenamento...">
                 </div>
             </div>
 
@@ -89,12 +89,12 @@
         <div class="col-md-6">
             <div class="form-group">
                 <label>Designação</label>
-                <input type="email" class="form-control" id="" placeholder="Digite a designação...">
+                <input type="text" class="form-control" id="" placeholder="Digite a designação...">
             </div>
 
             <div class="form-group">
                 <label>Stock minimo</label>
-                <input type="email" class="form-control" id="" placeholder="Digite o stock minimo...">
+                <input type="text" class="form-control" id="" placeholder="Digite o stock minimo...">
             </div>
 
             <!-- display none - lado direito -->
@@ -113,16 +113,18 @@
 
                 <div class="form-group">
 
-
                     <label>Pictogramas</label>
-                    <div class="input-group">
-                        <div class="custom-file">
-                            <input type="file" class="custom-file-input" id="exampleInputFile">
-                            <label class="custom-file-label" for="exampleInputFile">Escolh os pictogramas</label>
-                        </div>
-
-                        
-                    </div>
+                    <select class="select2 select2-hidden-accessible" id="selectPictograma" multiple="multiple" data-placeholder="Select a State" style="width: 100%;" data-select2-id="7" tabindex="-1" aria-hidden="true">
+                        <option data-select2-id="29">Pictograma 1</option>
+                        <option data-select2-id="30">Pictograma 2</option>
+                        <option data-select2-id="31">Pictograma 3</option>
+                        <option data-select2-id="32">Pictograma 4</option>
+                        <option data-select2-id="33">Pictograma 5</option>
+                        <option data-select2-id="34">Pictograma 6</option>
+                        <option data-select2-id="35">Pictograma 7</option>
+                    </select><span class="select2 select2-container select2-container--default select2-container--below" dir="ltr" data-select2-id="8" style="width: 100%;"><span class="selection"><span class="select2-selection select2-selection--multiple" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="-1" aria-disabled="false"><ul class="select2-selection__rendered"><li class="select2-selection__choice" title="California" data-select2-id="74"><span class="select2-selection__choice__remove" role="presentation">×</span>California</li><li class="select2-selection__choice" title="Delaware" data-select2-id="75"><span class="select2-selection__choice__remove" role="presentation">×</span>Delaware</li><li class="select2-search select2-search--inline"><input class="select2-search__field" type="search" tabindex="0" autocomplete="off" autocorrect="off" autocapitalize="none" spellcheck="false" role="searchbox" aria-autocomplete="list" placeholder="" style="width: 0.75em;"></li></ul></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
+                
+                </div>
 
 
 
@@ -130,16 +132,12 @@
 
                     <div class="form-group">
                         <label>Código de recomendação de prudência</label>
-             
-                        <input type="email" class="form-control" id="" placeholder="Digite o código de recomendação...">
-
+                        <input type="text" class="form-control" id="" placeholder="Digite o código de recomendação...">
                     </div>
-
-                    
 
                     <div class="form-group">
                         <label>Código de advertência de perigo</label>
-                        <input type="email" class="form-control" id="" placeholder="Digite o código de advertência...">
+                        <input type="text" class="form-control" id="" placeholder="Digite o código de advertência...">
                     </div>
 
                     <div class="form-check">
@@ -152,7 +150,7 @@
             <div class="naoQuimico1" style="display:none;">
                 <div class="form-group">
                     <label>Familia</label>
-                    <input type="email" class="form-control" id="" placeholder="Digite a família...">
+                    <input type="text" class="form-control" id="" placeholder="Digite a família...">
                 </div>
             </div>
         </div>
@@ -173,4 +171,11 @@
 
 @section('js')
     <script src="/js/adicionar.js"></script>
+
+    <script>
+        $(document).ready(function () {
+            $('selectPictograma').select2();
+        });
+    </script>
+
 @stop
