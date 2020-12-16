d8ba4e8c3ddafbfa72a203a39d25912cb3da1ee1
rafaelff97 <rFael_f_fernandes@hotmail.com>
2020-12-16 18:00:11 +0000

    página atualizada

--- projeto_tematico/resources/views/lista-produtos.blade.php
+++ projeto_tematico/resources/views/lista-produtos.blade.php
@@ -5,12 +5,12 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0 text-dark">Operadores</h1>
+                <h1 class="m-0 text-dark">Lista de produtos</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Operadores</li>
+                    <li class="breadcrumb-item active">Produtos</li>
                 </ol>
             </div>
         </div>
@@ -21,6 +21,15 @@
     Adicionar o botao no final para a ação como eliminar ou mudar de cargo ou assim
 -->
 @section('content')
+
+<ul id="menu" class="mfb-component--br mfb-slidein" data-mfb-toggle="hover">
+  <li class="mfb-component__wrap">
+    <a data-mfb-label="Nova entrada" class="mfb-component__button--main" href="/produtos/adicionar">
+      <i class="mfb-component__main-icon--resting fas fa-plus" style="font-size: 1.5rem;" ></i>
+    </a>
+  </li>
+</ul>
+
 <div class="card">
     <div class="card-body">
         <table id="table" class="table table-head-fixed">
@@ -77,8 +86,7 @@
                     <td>25g</td>
                     <td>G-3</td>
                     <td>Quimico</td>
-                    <td>
-                    </td>
+                    <td></td>
                 </tr>
                 <tr>
                     <td>Hidróxido de sódio</td>
@@ -104,14 +112,14 @@
                     <td>25g</td>
                     <td>G-3</td>
                     <td>Quimico</td>
-                    <td>
-                    </td>
+                    <td></td>
                 </tr>
                 </tfoot>
         </table>
     </div>
     <!-- /.card-body -->
 </div>
+
 <br>
 @stop
 
@@ -128,9 +136,18 @@
         "columnDefs": [ {
             "targets": -1,
             "data": null,
-            "defaultContent": '<div class="btn-group"><button type="button" class="btn btn-primary">Detalhes</button><button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split"data-toggle="dropdown"><span class="caret"></span></button><div class="dropdown-menu"><a class="dropdown-item" href="/produtos/info-produto">Informação do produto</a><a class="dropdown-item" href="/produtos/adicionar">Adicionar produto</a></div></div>'
+            "defaultContent": '<div class="btn-group"><button href="" type="button" class="btn btn-primary">Informação do produto</button></div>'
         }]
     });
   });
 </script>
 @stop
+
+@section('css')
+  <link rel="stylesheet" href="/css/custom.css">
+  <link href="/css/mfb.css" rel="stylesheet">
+@stop
+
+@section('js')
+  <script src="/js/mfb.js"></script>
+@endsection
