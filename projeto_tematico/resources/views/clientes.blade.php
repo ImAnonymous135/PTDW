1e02cef7f0d6d9bd2a7d93276d4a96e28d9f96f2
Gonçalo <g.junqueira@ua.pt>
2020-12-16 15:16:51 +0000

    página de clientes feita ig

--- projeto_tematico/resources/views/clientes.blade.php
+++ projeto_tematico/resources/views/clientes.blade.php
@@ -1,172 +1,168 @@
 @extends('adminlte::page')
 
 @section('content_header')
-<div class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="m-0 text-dark">Clientes</h1>
-            </div>
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Clientes</li>
-                </ol>
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark">Clientes</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Clientes</li>
+                    </ol>
+                </div>
             </div>
         </div>
     </div>
-</div>
+@stop
+<!--
+    Adicionar o botao no final para a ação como eliminar ou mudar de cargo ou assim
+-->
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <table id="table" class="table table-head-fixed">
+                <thead>
+                    <tr>
+                        <th>Designação</th>
+                        <th>Nome do responsavel</th>
+                        <th>E-mail do responsavel</th>
+                        <th>Nome do Solicitante</th>
+                        <th>E-mail do socilitante</th>
+                        <th>Observações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Sala 16.7</td>
+                        <td>Joaquim Ferreira</td>
+                        <td>j.quim@ua.pt</td>
+                        <td>Méci Tabuleiro</td>
+                        <td>Mci@ua.pt</td>
+                        <td>10/10 will work here</td>
+                    </tr>
+                    <tr>
+                        <td>Sala 16.7</td>
+                        <td>Joaquim Ferreira</td>
+                        <td>j.quim@ua.pt</td>
+                        <td>Méci Tabuleiro</td>
+                        <td>Mci@ua.pt</td>
+                        <td>10/10 will work here</td>
+                    </tr>
+                    <tr>
+                        <td>Sala 16.7</td>
+                        <td>Joaquim Ferreira</td>
+                        <td>j.quim@ua.pt</td>
+                        <td>Méci Tabuleiro</td>
+                        <td>Mci@ua.pt</td>
+                        <td>10/10 will work here</td>
+                    </tr>
+                    <tr>
+                        <td>Sala 16.7</td>
+                        <td>Joaquim Ferreira</td>
+                        <td>j.quim@ua.pt</td>
+                        <td>Méci Tabuleiro</td>
+                        <td>Mci@ua.pt</td>
+                        <td>10/10 will work here</td>
+                    </tr>
+                    <tr>
+                        <td>Sala 16.7</td>
+                        <td>Joaquim Ferreira</td>
+                        <td>j.quim@ua.pt</td>
+                        <td>Méci Tabuleiro</td>
+                        <td>Mci@ua.pt</td>
+                        <td>10/10 will work here</td>
+                    </tr>
+                    <tr>
+                        <td>Sala 16.7</td>
+                        <td>Joaquim Ferreira</td>
+                        <td>j.quim@ua.pt</td>
+                        <td>Méci Tabuleiro</td>
+                        <td>Mci@ua.pt</td>
+                        <td>10/10 will work here</td>
+                    </tr>
+                    <tr>
+                        <td>Sala 16.7</td>
+                        <td>Joaquim Ferreira</td>
+                        <td>j.quim@ua.pt</td>
+                        <td>Méci Tabuleiro</td>
+                        <td>Mci@ua.pt</td>
+                        <td>10/10 will work here</td>
+                    </tr>
+                    <tr>
+                        <td>Sala 16.7</td>
+                        <td>Joaquim Ferreira</td>
+                        <td>j.quim@ua.pt</td>
+                        <td>Méci Tabuleiro</td>
+                        <td>Mci@ua.pt</td>
+                        <td>10/10 will work here</td>
+                    </tr>
+                    <tr>
+                        <td>Sala 16.7</td>
+                        <td>Joaquim Ferreira</td>
+                        <td>j.quim@ua.pt</td>
+                        <td>Méci Tabuleiro</td>
+                        <td>Mci@ua.pt</td>
+                        <td>10/10 will work here</td>
+                    </tr>
+                    <tr>
+                        <td>Sala 16.7</td>
+                        <td>Joaquim Ferreira</td>
+                        <td>j.quim@ua.pt</td>
+                        <td>Méci Tabuleiro</td>
+                        <td>Mci@ua.pt</td>
+                        <td>10/10 will work here</td>
+                    </tr>
+                    <tr>
+                        <td>Sala 16.7</td>
+                        <td>Joaquim Ferreira</td>
+                        <td>j.quim@ua.pt</td>
+                        <td>Méci Tabuleiro</td>
+                        <td>Mci@ua.pt</td>
+                        <td>10/10 will work here</td>
+                    </tr>
+                    <tr>
+                        <td>Sala 16.7</td>
+                        <td>Joaquim Ferreira</td>
+                        <td>j.quim@ua.pt</td>
+                        <td>Méci Tabuleiro</td>
+                        <td>Mci@ua.pt</td>
+                        <td>10/10 will work here</td>
+                    </tr>
+                    <tr>
+                        <td>Sala 16.7</td>
+                        <td>Joaquim Ferreira</td>
+                        <td>j.quim@ua.pt</td>
+                        <td>Méci Tabuleiro</td>
+                        <td>Mci@ua.pt</td>
+                        <td>10/10 will work here</td>
+                    </tr>
+                    </tfoot>
+            </table>
+        </div>
+        <!-- /.card-body -->
+    </div>
+    <br>
 @stop
 
-@section('content')
-<div class="card">
-    <div class="card-body">
-        <table id="table" class="table table-head-fixed">
-            <thead>
-                <tr>
-                    <th>To do</th>
-                    <th>To dol</th>
-                    <th>To do</th>
-                    <th>To do</th>
-                    <th>To do</th>
-                    <th>To do</th>
-                    <th>To do</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Aluno 1</td>
-                    <td>Aluno 1@ua.pt</td>
-                    <td>Fiel de armazém</td>
-                    <td>14/12/2020</td>
-                    <td>15/12/2020</td>
-                    <td>Observação</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Aluno 1</td>
-                    <td>Aluno 1@ua.pt</td>
-                    <td>Fiel de armazém</td>
-                    <td>14/12/2020</td>
-                    <td>15/12/2020</td>
-                    <td>Observação</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Aluno 1</td>
-                    <td>Aluno 1@ua.pt</td>
-                    <td>Fiel de armazém</td>
-                    <td>14/12/2020</td>
-                    <td>15/12/2020</td>
-                    <td>Observação</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Aluno 1</td>
-                    <td>Aluno 1@ua.pt</td>
-                    <td>Fiel de armazém</td>
-                    <td>14/12/2020</td>
-                    <td>15/12/2020</td>
-                    <td>Observação</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Aluno 1</td>
-                    <td>Aluno 1@ua.pt</td>
-                    <td>Fiel de armazém</td>
-                    <td>14/12/2020</td>
-                    <td>15/12/2020</td>
-                    <td>Observação</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Aluno 1</td>
-                    <td>Aluno 1@ua.pt</td>
-                    <td>Fiel de armazém</td>
-                    <td>14/12/2020</td>
-                    <td>15/12/2020</td>
-                    <td>Observação</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Aluno 1</td>
-                    <td>Aluno 1@ua.pt</td>
-                    <td>Fiel de armazém</td>
-                    <td>14/12/2020</td>
-                    <td>15/12/2020</td>
-                    <td>Observação</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Aluno 1</td>
-                    <td>Aluno 1@ua.pt</td>
-                    <td>Fiel de armazém</td>
-                    <td>14/12/2020</td>
-                    <td>15/12/2020</td>
-                    <td>Observação</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Aluno 1</td>
-                    <td>Aluno 1@ua.pt</td>
-                    <td>Fiel de armazém</td>
-                    <td>14/12/2020</td>
-                    <td>15/12/2020</td>
-                    <td>Observação</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Aluno 1</td>
-                    <td>Aluno 1@ua.pt</td>
-                    <td>Fiel de armazém</td>
-                    <td>14/12/2020</td>
-                    <td>15/12/2020</td>
-                    <td>Observação</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Aluno 1</td>
-                    <td>Aluno 1@ua.pt</td>
-                    <td>Fiel de armazém</td>
-                    <td>14/12/2020</td>
-                    <td>15/12/2020</td>
-                    <td>Observação</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Aluno 1</td>
-                    <td>Aluno 1@ua.pt</td>
-                    <td>Fiel de armazém</td>
-                    <td>14/12/2020</td>
-                    <td>15/12/2020</td>
-                    <td>Observação</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Aluno 1</td>
-                    <td>Aluno 1@ua.pt</td>
-                    <td>Fiel de armazém</td>
-                    <td>14/12/2020</td>
-                    <td>15/12/2020</td>
-                    <td>Observação</td>
-                    <td></td>
-                </tr>
-                </tfoot>
-        </table>
-    </div>
-    <!-- /.card-body -->
-</div>
-<br>
-@stop
 
 @section('js')
-<script>
-    $(function () {
-    $('#table').DataTable({
-      "responsive": true,
-      "autoWidth": false,
-      "ordering": false,
-    });
-  });
-</script>
+    <script src="js/mfb.js"></script>
+    {{-- Data table --}}
+    <script>
+        $(function() {
+            $('#table').DataTable({
+                "responsive": true,
+                "autoWidth": false,
+            });
+        });
+
+    </script>
+    @include('sub-views.exports')
 @stop
+
+@section('css')
+    <link href="css/mfb.css" rel="stylesheet">
+@endsection
