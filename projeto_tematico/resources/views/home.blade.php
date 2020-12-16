4d505b723ef3e9b03b1ef4171e1550b48fbd4478
rafaelff97 <rFael_f_fernandes@hotmail.com>
2020-12-16 16:50:16 +0000

    Página inicial + alterações

---
+++ projeto_tematico/resources/views/home.blade.php
@@ -0,0 +1,29 @@
+@extends('adminlte::page')
+
+@section('content_header')
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0 text-dark">Home</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
+<!--
+    Adicionar o botão em baixo a direita como o tem na pagina de produto no registo para adicionar um novo fornecedor
+    Como ainda ha muita informaçao para por é melhor por um botao com mais informaçoes ou algo do genero
+-->
+@section('content')
+<div class="card">
+    teste
+</div>
+<br>
+@stop
+
