bbe390d82dd9991a0a9e071eea78abe57fa9d7c8
rafaelff97 <rFael_f_fernandes@hotmail.com>
2020-12-17 18:03:49 +0000

    Página feitas para o servidor

--- projeto_tematico/resources/views/estilos.blade.php
+++ projeto_tematico/resources/views/estilos.blade.php
@@ -171,12 +171,10 @@
                     <td>3750-141</td>
                     <td>912478356</td>
                     <td>215478543</td>
-                    <td><button type="button" class="btn btn-primary">Detalhes</button></td>
+                    <td><button type="button" class="btn btn-primary toastrDefaultSuccess1">Detalhes</button></td>
                 </tr>
                 </tfoot>
         </table>
-    </div>
-    <div class="card-body">
     </div>
 </div>
 <br>
@@ -206,41 +204,181 @@
         </tfoot>
     </table>
     </div>
+</div>
+
+<div class="card">
+    <div class="card-header">
+        <h4>TextArea</h4>
+        <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+    </div>
     <div class="card-body">
     </div>
 </div>
-@stop
 
-@section('css')
-<link href="/css/mfb.css" rel="stylesheet">
-<style>
-    #menu {
-        position: unset;
-    }
-</style>
+<div class="modal fade" id="modal-default1" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Editar fornecedor</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body w-200">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Nome</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite o nome...">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Morada</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite a morada...">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Localidade</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite a localidade...">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Código-postal</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite o código-postal...">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Telefone</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite o telefone...">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>NIF</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite o NIF...">
+                        </div>
+                    </div>
+                </div>            
+
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default toastrDefaultSuccess1" data-dismiss="modal" >Cancelar</button>
+                <button type="button" class="btn btn-primary toastrDefaultSuccess1" data-dismiss="modal">Guardar</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+
+
+<ul id="menu" class="mfb-component--br mfb-slidein" data-mfb-toggle="hover">
+    <li class="mfb-component__wrap">
+        <a data-mfb-label="Novo fornecedor" class="mfb-component__button--main" href="/produtos/adicionar">
+            <i class="mfb-component__main-icon--resting fas fa-plus" style="font-size: 1.5rem;"></i>
+        </a>
+    </li>
+</ul>
+
+<div class="card">
+    <div class="card-body">
+        <table id="table" class="table table-head-fixed">
+            <thead>
+                <tr>
+                    <th>Fornecedor</th>
+                    <th>Morada</th>
+                    <th>Localidade</th>
+                    <th>Código Postal</th>
+                    <th>Telefone</th>
+                    <th>NIF</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Quimifeira</td>
+                    <td>Rua Quim Pascal</td>
+                    <td>Águeda</td>
+                    <td>3750-141</td>
+                    <td>912478356</td>
+                    <td>215478543</td>
+                    <td><button type="button" class="btn btn-primary">Detalhes</button></td>
+                </tr>
+                </tfoot>
+        </table>
+        <br>
+    </div>
+    <!-- /.card-body -->
+</div>
+<br>
+<div class="modal fade" id="modal-default" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">Eliminar</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>Pretende eliminar o fornecedor?</p>
+        </div>
+        <div class="modal-footer justify-content-between">
+          <button type="button" class="btn btn-default toastrDefaultSuccess1" data-dismiss="modal">Não</button>
+          <button type="button" class="btn btn-primary toastrDefaultSuccess1" data-dismiss="modal">Sim</button>
+        </div>
+      </div>
+      <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+  </div>
+</div>
 @stop
 
 @section('js')
+
 <script src="/js/mfb.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
 <script>
+
+    $('.toastrDefaultSuccess1').click(function() {
+      toastr.success('Editado com sucesso.')
+    });
+
     $(function () {
-    $('#table1').DataTable({
-      "responsive": true,
-      "autoWidth": false,
-      "ordering": false,
-      language: {
-            url: '//cdn.datatables.net/plug-ins/1.10.22/i18n/Portuguese.json'
+    $('#table').DataTable({
+        "responsive": true,
+        "autoWidth": false,
+        language: {
+                url: '//cdn.datatables.net/plug-ins/1.10.22/i18n/Portuguese.json'
         },
+        "columnDefs": [ {
+            "targets": -1,
+            "data": null,
+            "defaultContent": '<div class="btn-group"><button type="button" class="btn btn-primary">Detalhes</button><button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split"data-toggle="dropdown"><span class="caret"></span></button><div class="dropdown-menu"><a class="dropdown-item" data-toggle="modal" data-target="#modal-default1" href="#">Editar</a><a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-default" href="#">Eliminar fornecedores</a></div></div>'
+        }]
+    });
+    $('.toastrDefaultSuccess').click(function() {
+      toastr.success('Eliminado com sucesso.')
     });
   });
-  $(function () {
-    $('#table2').DataTable({
-      "responsive": true,
-      "autoWidth": false,
-      language: {
-            url: '//cdn.datatables.net/plug-ins/1.10.22/i18n/Portuguese.json'
-        },
-    });
-  });
+
 </script>
-@endsection
+@stop
+
+@section('css')
+<link rel="stylesheet" href="/css/custom.css">
+<link href="/css/mfb.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
+@stop
--- projeto_tematico/resources/views/fornecedores.blade.php
+++ projeto_tematico/resources/views/fornecedores.blade.php
@@ -120,6 +120,139 @@
     </div>
     <!-- /.modal-dialog -->
   </div>
+
+  <div class="modal fade" id="modal-default1" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Editar fornecedor</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body w-200">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Nome</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite o nome...">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Morada</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite a morada...">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Localidade</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite a localidade...">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Código-postal</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite o código-postal...">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Telefone</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite o telefone...">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>NIF</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite o NIF...">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>E-mail</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite o e-mail...">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Vendedor 1</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite o nome do vendedor 1...">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Telemovél 1</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite o telemóvel 1...">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>E-mail 1</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite o e-mail 1...">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Vendedor 2</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite o nome vendedor 2...">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Telemóvel 2</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite o telemóvel 2...">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>E-mail 2</label>
+                            <input type="text" class="form-control" id="#" placeholder="Digite o e-mail 2...">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Observações</label>
+                            <textarea class="form-control" rows="3" placeholder="Digite as observações.."></textarea>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Condições especiais</label>
+                            <textarea class="form-control" rows="3" placeholder="Digite as condições especiais.."></textarea>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-primary toastrDefaultSuccess1" data-dismiss="modal">Guardar</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
 @stop
 
 @section('js')
@@ -128,6 +261,11 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
 <script>
+
+    $('.toastrDefaultSuccess1').click(function() {
+      toastr.success('Editado com sucesso.')
+    });
+
     $(function () {
     $('#table').DataTable({
         "responsive": true,
@@ -138,7 +276,7 @@
         "columnDefs": [ {
             "targets": -1,
             "data": null,
-            "defaultContent": '<div class="btn-group"><button type="button" class="btn btn-primary">Detalhes</button><button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split"data-toggle="dropdown"><span class="caret"></span></button><div class="dropdown-menu"><a class="dropdown-item" href="#">Editar</a><a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-default" href="#">Eliminar fornecedores</a></div></div>'
+            "defaultContent": '<div class="btn-group"><button type="button" class="btn btn-primary">Detalhes</button><button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split"data-toggle="dropdown"><span class="caret"></span></button><div class="dropdown-menu"><a class="dropdown-item" data-toggle="modal" data-target="#modal-default1" href="#">Editar</a><a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-default" href="#">Eliminar fornecedores</a></div></div>'
         }]
     });
     $('.toastrDefaultSuccess').click(function() {
