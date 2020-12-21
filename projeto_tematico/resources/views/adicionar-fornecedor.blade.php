ab81021914a50b5cdf5881e072cf1f0feb144977
rafaelff97 <rFael_f_fernandes@hotmail.com>
2020-12-21 20:50:15 +0000

    página dos fornecedores

--- projeto_tematico/resources/views/adicionar-fornecedor.blade.php
+++ projeto_tematico/resources/views/adicionar-fornecedor.blade.php
@@ -6,13 +6,13 @@
         <div class="row mb-2">
             <div class="col-sm-6">
                 <h1 class="m-0 text-dark">
-                    Adicionar cliente
+                    Adicionar fornecedor
                 </h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item "><a href="/clientes">Clientes</a></li>
+                    <li class="breadcrumb-item "><a href="/fornecedor">Fornecedor</a></li>
                     <li class="breadcrumb-item active">Adicionar</li>
                 </ol>
             </div>
@@ -32,33 +32,90 @@
                     <input type="text" class="form-control" id="#" placeholder="Digite a designação...">
                 </div>
             </div>
-
-        </div>
-        <div class="row">
             <div class="col-sm-4">
                 <div class="form-group">
-                    <label>Nome do responsável</label>
-                    <input type="text" class="form-control" id="#" placeholder="Digite o nome do responsável...">
+                    <label>Morada</label>
+                    <input type="text" class="form-control" id="#" placeholder="Digite a morada...">
                 </div>
             </div>
             <div class="col-sm-4">
                 <div class="form-group">
-                    <label>E-mail do responsável</label>
-                    <input type="text" class="form-control" id="#" placeholder="Digite o e-mail do responsável..">
+                    <label>Localidade</label>
+                    <input type="text" class="form-control" id="#" placeholder="Digite a localidade...">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-2">
+                <div class="form-group">
+                    <label>Código Postal</label>
+                    <input type="text" class="form-control" id="#" placeholder="Digite o código postal...">
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <div class="form-group">
+                    <label>Telefone</label>
+                    <input type="text" class="form-control" id="#" placeholder="Digite o telefone...">
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <div class="form-group">
+                    <label>NIF</label>
+                    <input type="text" class="form-control" id="#" placeholder="Digite o NIF...">
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="form-group">
+                    <label>E-Mail</label>
+                    <input type="text" class="form-control" id="#" placeholder="Digite o e-mail...">
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-3">
                 <div class="form-group">
-                    <label>Nome do solicitante</label>
-                    <input type="text" class="form-control" id="#" placeholder="Digite o nome do solicitante...">
+                    <label>Vendedor 1</label>
+                    <input type="text" class="form-control" id="#" placeholder="Digite o vendedor 1...">
                 </div>
             </div>
+            <div class="col-sm-2">
+                <div class="form-group">
+                    <label>Telemóvel 1</label>
+                    <input type="text" class="form-control" id="#" placeholder="Digite o telemóvel 1...">
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="form-group">
+                    <label>E-Mail 1</label>
+                    <input type="text" class="form-control" id="#" placeholder="Digite o e-mail 1...">
+                </div>
+            </div>
+        </div>
+        <div class="row">
             <div class="col-sm-3">
                 <div class="form-group">
-                    <label>E-mail do solicitante</label>
-                    <input type="text" class="form-control" id="#" placeholder="Digite o e-mail do solicitante...">
+                    <label>Vendedor 2</label>
+                    <input type="text" class="form-control" id="#" placeholder="Digite o vendedor 2...">
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <div class="form-group">
+                    <label>Telemóvel 2</label>
+                    <input type="text" class="form-control" id="#" placeholder="Digite o telemóvel 2...">
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="form-group">
+                    <label>E-Mail 2</label>
+                    <input type="text" class="form-control" id="#" placeholder="Digite o e-mail 2...">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="form-group">
+                    <label>Condições especiais</label>
+                    <input type="text" class="form-control" id="#" placeholder="Digite o condições especiais...">
                 </div>
             </div>
         </div>
@@ -70,6 +127,7 @@
                 </div>
             </div>
         </div>
+       
         <div>
             <div class="d-flex flex-row justify-content-end">
                 <span class="mr-2">
