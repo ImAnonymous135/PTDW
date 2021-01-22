d0ef675baabfa4a6525fffbd434dd88ee084ef7a
João <joaobettencourt2000@hotmail.com>
2021-01-22 21:32:52 +0000

    Fix

--- projeto_tematico/app/Models/Armario.php
+++ projeto_tematico/app/Models/Armario.php
@@ -8,10 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Armario extends Model
 {
     protected $table = 'armario';
-<<<<<<< Updated upstream
     
-=======
->>>>>>> Stashed changes
     public function cliente(){
         return $this->belongsTo(Cliente::class,'id_cliente');
     }
